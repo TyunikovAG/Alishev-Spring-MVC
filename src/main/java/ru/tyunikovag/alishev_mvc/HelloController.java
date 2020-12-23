c69785bb35b1754285fdcd40b59bf871ac90233b
Tyunikov <tyunikovag@gmail.com>
2020-12-24 02:09:38 +0300

    Init commit

---
+++ src/main/java/ru/tyunikovag/alishev_mvc/HelloController.java
@@ -0,0 +1,13 @@
+package ru.tyunikovag.alishev_mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello_ass";
+    }
+}
